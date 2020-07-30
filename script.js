1395950a8759950961dd2963be6876bf07b126b3
Tor Smedberg <tor.smedberg@gmail.com>
2020-07-30 12:07:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-document.getElementById("copyright").innerHTML = "Copyright Tor Smedberg" + Date.getYear();
+var d = new Date();
+document.getElementById("copyright").innerHTML = "Copyright Tor Smedberg" + d.getFullYear();
+console.log("Changed copyright date to" + d.getFullYear());
