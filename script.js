4729bc47179ee8343f42ebda6c972d383df512bf
Tor Smedberg <tor.smedberg@gmail.com>
2020-09-17 20:15:26 +0200

    Added navbar

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var mainNavbarExpanded = false;
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/sw.js')
     .then(function(registration) {
@@ -7,4 +8,19 @@ if ('serviceWorker' in navigator) {
       console.log('Service worker registration failed, error:', error);
     });
   }
-  
+  function navbarToggle()
+  {
+    var navbar = document.getElementById("mainNavbar")
+    var navbarList = document.getElementById("mainNavbarList")
+    if (!mainNavbarExpanded)
+    {
+      mainNavbarExpanded = true;
+      navbar.style.animation = "navbarExpand 1s forwards";
+      navbarList.style.animation = "navbarListExpand 0s forwards";
+    }
+    else {
+      mainNavbarExpanded = false;
+      navbarList.style.animation = "navbarListContract 0s forwards";
+      navbar.style.animation = "navbarContract 1s forwards";
+    }
+  }
