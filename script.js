5a1f71098faebe4388e05a622943123789e58638
Tor Smedberg <tor.smedberg@gmail.com>
2020-10-08 10:50:05 +0200

    Fixed navbar, was accidentally changed before.

--- script.js
+++ script.js
@@ -18,11 +18,11 @@ if ('serviceWorker' in navigator) {
     {
       mainNavbarExpanded = true;
       navbar.style.animation = "navbarExpand 1s forwards";
-      //navbarList.style.animation = "navbarListExpand 1s forwards";
+      navbarList.style.animation = "navbarListExpand 1s forwards";
     }
     else {
       mainNavbarExpanded = false;
       navbarList.style.animation = "navbarListContract 1s forwards";
-      //navbar.style.animation = "navbarContract 1s forwards";
+      navbar.style.animation = "navbarContract 1s forwards";
     }
   }
